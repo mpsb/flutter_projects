7ffc9d209f6ae7b324a262e7cff8b2e6e7fc46bb
mpsb <mps.bilo@gmail.com>
2020-07-24 12:36:12 +0800

    finished conversion logic. finished app.

--- unit_converter/lib/main.dart
+++ unit_converter/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   final Map<String, int> _measuresMap = {
-    'meters':0,
+    'meters': 0,
     'kilometers': 1,
     'grams': 2,
     'kilograms': 3,
@@ -20,10 +20,17 @@ class MyAppState extends State<MyApp> {
     'pounds (lbs)': 6,
     'ounces': 7,
   };
-  
-  double _numberFrom;
-  String _startMeasure;
-  String _convertedMeasure;
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.92, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
 
   final List<String> _measures = [
     'meters',
@@ -35,6 +42,11 @@ class MyAppState extends State<MyApp> {
     'pounds (lbs)',
     'ounces',
   ];
+
+  double _numberFrom;
+  String _startMeasure;
+  String _convertedMeasure;
+  String _resultMessage;
 
   @override
   void initState() {
@@ -131,13 +143,20 @@ class MyAppState extends State<MyApp> {
                   'Convert',
                   style: inputStyle,
                 ),
-                onPressed: () => true,
+                onPressed: () {
+                  if (_startMeasure.isEmpty || _convertedMeasure.isEmpty || _numberFrom == 0) {
+                    return;
+                  }
+                  else {
+                    convert(_numberFrom, _startMeasure, _convertedMeasure);
+                  }
+                },
               ),
               Spacer(
                 flex: 2,
               ),
               Text(
-                (_numberFrom == null) ? '' : _numberFrom.toString(),
+                (_resultMessage == null) ? '' : _resultMessage.toString(),
                 style: labelStyle,
               ),
               Spacer(
@@ -148,5 +167,23 @@ class MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+
+  void convert(double value, String from, String to) {
+    int nFrom = _measuresMap[from];
+    int nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed.';
+    }
+    else {
+      _resultMessage = '${_numberFrom.toString()} $_startMeasure is ${result.toString()} $_convertedMeasure';
+    }
+
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
   }
 }
