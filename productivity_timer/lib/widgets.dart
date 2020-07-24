30e990e71b0726f05d079b19c01aeb90b4e44325
mpsb <mps.bilo@gmail.com>
2020-07-24 16:27:47 +0800

    setting up initial widgets.

---
+++ productivity_timer/lib/widgets.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProductivityButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double size;
+  final VoidCallback onPressed;
+
+  ProductivityButton(
+    this.color,
+    this.text,
+    this.size,
+    this.onPressed,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text(
+        this.text,
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: this.onPressed,
+      color: this.color,
+      minWidth: this.size,
+    );
+  }
+}
