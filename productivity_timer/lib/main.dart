b97536c8e0baab882de0a11941343ece68410c22
mpsb <mps.bilo@gmail.com>
2020-07-25 12:21:11 +0800

    created settings page and route from home to settings page.

--- productivity_timer/lib/main.dart
+++ productivity_timer/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:work_timer/settings.dart';
 import 'package:work_timer/timermodel.dart';
 import 'package:work_timer/widgets.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import './timer.dart';
+import './settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,11 +26,33 @@ class MyApp extends StatelessWidget {
 class TimerHomePage extends StatelessWidget {
   final double defaultPadding = 5.0;
 
+  void goToSettings(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<PopupMenuItem<String>> menuItems = List<PopupMenuItem<String>>();
+    menuItems.add(PopupMenuItem(
+      value: 'Settings',
+      child: Text('Settings'),
+    ));
+
     return Scaffold(
         appBar: AppBar(
           title: Text('My Work Timer'),
+          actions: [
+            PopupMenuButton<String>(
+              itemBuilder: (BuildContext context) {
+                return menuItems.toList();
+              },
+              onSelected: (s) {
+                if (s=='Settings') {
+                  goToSettings(context);
+                }
+              },
+            )
+          ],
         ),
         body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
---
+++ productivity_timer/lib/settings.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Container(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
