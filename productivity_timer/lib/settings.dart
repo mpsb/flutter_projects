ac9331f112fd2103f7ea8b3be85e3a217f1478e1
mpsb <mps.bilo@gmail.com>
2020-07-25 12:53:16 +0800

    finished settings UI.

--- productivity_timer/lib/settings.dart
+++ productivity_timer/lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:work_timer/widgets.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -7,9 +8,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Container(
-        child: Text('Hello World'),
-      ),
+      body: Settings()
     );
   }
 }
@@ -20,9 +19,60 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  TextStyle textStyle = TextStyle(fontSize: 24);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: GridView.count(
+        scrollDirection: Axis.vertical,
+        crossAxisCount: 3,
+        childAspectRatio: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: <Widget>[
+          Text(
+            "Work",
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingsButton(Color(0xff455a64), "-", -1),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+          ),
+          SettingsButton(Color(0xff009688), "+", 1),
+          Text(
+            "Short",
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingsButton(Color(0xff455a64), "-", -1),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+          ),
+          SettingsButton(Color(0xff009688), "+", 1),
+          Text(
+            "Long",
+            style: textStyle,
+          ),
+          Text(''),
+          Text(''),
+          SettingsButton(Color(0xff455a64), "-", -1),
+          TextField(
+            style: textStyle,
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+          ),
+          SettingsButton(Color(0xff009688), "+", 1),
+        ],
+        padding: const EdgeInsets.all(20),
+      ),
+    );
   }
 }
-
--- productivity_timer/lib/widgets.dart
+++ productivity_timer/lib/widgets.dart
@@ -9,8 +9,8 @@ class ProductivityButton extends StatelessWidget {
   ProductivityButton({
     @required this.color,
     @required this.text,
-    this.size,
     @required this.onPressed,
+    this.size,
   });
 
   @override
@@ -23,6 +23,26 @@ class ProductivityButton extends StatelessWidget {
       onPressed: this.onPressed,
       color: this.color,
       minWidth: this.size,
+    );
+  }
+}
+
+class SettingsButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final int value;
+
+  SettingsButton(this.color, this.text, this.value);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: Text(
+        this.text,
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () => null,
+      color: this.color,
     );
   }
 }
