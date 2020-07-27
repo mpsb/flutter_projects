a28d256cccdd1a5cfa51871cd7829a5d068000e2
mpsb <mps.bilo@gmail.com>
2020-07-27 15:40:49 +0800

    created ball and bat UI elements.

--- pong_game/lib/main.dart
+++ pong_game/lib/main.dart
@@ -7,6 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Pong Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pong Game'),
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
