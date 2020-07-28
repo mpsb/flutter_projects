d9bd4f0e7a352af1b2ae6216ca7c2634bedbf11b
mpsb <mps.bilo@gmail.com>
2020-07-28 09:23:20 +0800

    added complete ball physics.

--- pong_game/lib/pong.dart
+++ pong_game/lib/pong.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:pong_game/ball.dart';
 import 'package:pong_game/bat.dart';
 
+enum Direction { up, down, left, right }
+
 class Pong extends StatefulWidget {
   @override
   _PongState createState() => _PongState();
 }
 
 class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
+  Direction vDir = Direction.down;
+  Direction hDir = Direction.right;
   Animation<double> animation;
   AnimationController controller;
+  double increment = 5;
   double width;
   double height;
   double posX = 0;
@@ -18,20 +23,36 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   double batHeight = 0;
   double batPosition = 0;
 
+  void checkBorders() {
+    if (posX <= 0 && hDir == Direction.left) {
+      hDir = Direction.right;
+    }
+    if (posX >= width - 50 && hDir == Direction.right) {
+      hDir = Direction.left;
+    }
+    if (posY >= height - 50 && vDir == Direction.down) {
+      vDir = Direction.up;
+    }
+    if (posY <= 0 && vDir == Direction.up) {
+      vDir = Direction.down;
+    }
+  }
+
   @override
   void initState() {
     posX = 0;
     posY = 0;
     controller = AnimationController(
-      duration: const Duration(seconds: 3),
+      duration: const Duration(seconds: 10000),
       vsync: this,
     );
     animation = Tween<double>(begin: 0, end: 100).animate(controller);
     animation.addListener(() {
       setState(() {
-        posX++;
-        posY++;
+        (hDir == Direction.right) ? posX+= increment : posX -= increment;
+        (vDir == Direction.down) ? posY+= increment : posY -= increment;
       });
+      checkBorders();
     });
     controller.forward();
     super.initState();
