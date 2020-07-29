f6eba907d554b4376e8b96cfa2b1112a9547fb9f
mpsb <mps.bilo@gmail.com>
2020-07-29 13:48:47 +0800

    tested api by getting upcoming movies. works.

--- movies_db/lib/http_helper.dart
+++ movies_db/lib/http_helper.dart
@@ -1,5 +1,23 @@
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
 class HttpHelper {
-  
+  final String urlKey = 'api_key=abd9090ade0ce3e6a5a93b706f27fffb';
+  final String urlBase = 'https://api.themoviedb.org/3/movie';
+  final String urlUpcoming = '/upcoming?';
+  final String urlLanguage = '&language=en-US';
+
+  Future<String> getUpcoming() async {
+    final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
+
+    http.Response result = await http.get(upcoming);
+
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+      return responseBody;
+    }
+    else {
+      return null;
+    }
+  }
 }
