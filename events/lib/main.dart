bc2f275838859af2008b690ef182798d2364c26b
mpsb <mps.bilo@gmail.com>
2020-08-05 10:31:47 +0800

    tested Firebase integration with testData().

--- events/lib/main.dart
+++ events/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+Future testData() async {
+  Firestore db = Firestore.instance;
+  var data = await db.collection('event_details').getDocuments();
+  var details = data.documents.toList();
+
+  details.forEach((d) {
+    print(d.documentID);
+  });
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    testData();
+
     return MaterialApp(
       title: 'Events',
       theme: ThemeData(
